e761672c23a362d01b660289d022460b7aaf3247
Yanay Goor <yanay.goor@gmail.com>
2022-04-23 15:48:48 -0700

    Add LineLimitFilter

---
+++ antigen/filters/line_limit.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from itertools import count
+from pathlib import Path
+from typing import Dict, Tuple
+
+from ..mutation import Mutation
+
+
+@dataclass
+class LineLimitFilter:
+    limit: int = 2
+    _lines: Dict[Tuple[Path, int], count] = field(init=False, default_factory=dict)
+
+    def __call__(self, mutation: Mutation) -> bool:
+        key = (mutation.context.file.path, mutation.context.node.lineno)
+        return next(self._lines.setdefault(key, count())) < self.limit
